cd85dc34b831d3fa015f649592d03a6cf3d70bb6
parkce <parkce3@gmail.com>
2024-08-22 01:41:44 +0900

    update

--- promptor/__init__.py
+++ promptor/__init__.py
@@ -1,3 +1,3 @@
 from .promptor_interface import PromptorInterface
 from .promptor import Promptor, ExaonePromptor, ChatGPTPromptor
-from .promptor.mk_instruction import mk_inst_for_vqa
+from .mk_instruction import mk_inst_for_vqa
