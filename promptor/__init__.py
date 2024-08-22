d03248116f076cefa823b15721b7c09e6ee40b17
parkce <parkce3@gmail.com>
2024-08-23 00:36:54 +0900

    prediction data loader

--- promptor/__init__.py
+++ promptor/__init__.py
@@ -1,3 +1,3 @@
 from .promptor_interface import PromptorInterface
 from .promptor import Promptor, ExaonePromptor, ChatGPTPromptor, LLaVAPromptor
-from .mk_instruction import mk_inst_for_vqa
+from .mk_instruction import mk_inst_for_vqa, mk_vqa_for_multiple_choice
