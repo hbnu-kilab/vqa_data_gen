7a4a87504241321aba6c12765d7e399a43a60f2a
parkce <parkce3@gmail.com>
2024-08-23 01:26:48 +0900

    prediction data loader

--- do_vqa.py
+++ do_vqa.py
@@ -80,9 +80,9 @@ def baseline(model_type, ex_lst):
             
             print(f"[EX-BEGIN: {mid}]\nQUESTION: {mc_question}\nANSWER: {mc_answer}")
             print(f"PRED_ANSWER: {pred_ans}\nEXACT_ANSWER: {exact_cnt}\tCHOICE_ANSWER: {choice_cnt}\n[DONE: {mid}]\n\n")
-            pf.write(f"[EX-BEGIN: {mid}]\nQUESTION: {mc_question}\nANSWER: {mc_answer}")
+            pf.write(f"[EX-BEGIN: {mid}]\nQUESTION: {mc_question}\nANSWER: {mc_answer}\n")
             pf.write(f"PRED_ANSWER: {pred_ans}\nEXACT_ANSWER: {exact_cnt}\tCHOICE_ANSWER: {choice_cnt}\n[DONE: {mid}]\n\n")
-            pf.write(f"<LOG>{output_vqa}</LOG>")
+            pf.write(f"<LOG>{output_vqa}</LOG>\n\n")
 
     print(f"SCORE: {choice_cnt/len(ex_lst)}")
     print(f"EXACT SCORE: {exact_cnt/len(ex_lst)}")
